e3c9489bb1340fc1ae9ea42c285429d0c1ec3d0d
manoj <manokumar2684@gmail.com>
2022-09-27 06:32:15 +0530

    Fibonacci Series without recursion

---
+++ Day#4/Program#4.go
@@ -0,0 +1,19 @@
+// Fibonacci Series
+package main
+
+import "fmt"
+
+var a, b int
+var n1, n2 int = 0, 1
+
+func main() {
+	fmt.Println("Till where you need the series:")
+	fmt.Scanln(&a)
+	for i := 2; i <= a+1; i++ {
+		b = n1 + n2
+		fmt.Println("Value of b is", b)
+		n1 = n2
+		n2 = b
+
+	}
+}
